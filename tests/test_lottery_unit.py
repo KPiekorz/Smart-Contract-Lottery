80d37b657d77b975a832a84d8f5443e8d713380e
kpiekorz <kpiekorz>
2022-01-24 21:19:30 +0000

    update

--- tests/test_lottery_unit.py
+++ tests/test_lottery_unit.py
@@ -4,13 +4,5 @@ from web3 import Web3
 from scripts.deploy_lottery import deploy_lottery
 
 def test_get_entrance_fee():
-    account = accounts[0]
-    lottery = Lottery.deploy(
-        config["networks"][network.show_active()]["eth_usd_price_feed"],
-        {"from": account}
-    )
-    print(Web3.toWei(0.015, "ether"))
-    print(Web3.toWei(0.025, "ether"))
-    print(lottery.getEntranceFee())
-    assert lottery.getEntranceFee() > Web3.toWei(0.015, "ether")
-    assert lottery.getEntranceFee() < Web3.toWei(0.025, "ether")
+    lottery = deploy_lottery()
+    entrence_fee = lottery.getEntranceFee()
